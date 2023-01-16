89e0613c1caacba5f461065bb3acfa29b1c7791c
goutam singh <goutamsinghchouhan32@gmail.com>
2023-01-16 10:09:10 +0530

    polymorphism

---
+++ PolyMorphism/Polymorphism.java
@@ -0,0 +1,37 @@
+
+class A{
+    public void show(){
+        System.out.println("in A");
+    }
+}
+class B extends A{
+    public void show(){
+        System.out.println("in B ");
+    }
+}
+class C extends A{
+    public void show(){
+        System.out.println("in C");
+    }
+} 
+class D{
+    public void show(){
+        System.out.println("in D");
+    }
+}
+
+public class Polymorphism {
+    public static void main(String args[]){
+
+        A obj = new A();
+        obj.show();          // in A
+
+        obj = new B();
+        obj.show();         // in B
+
+        obj = new C();
+        obj.show();         //in C
+
+        // obj = new D();      // giver error -> as obj is of type A , and D does not extends A
+    }
+}
