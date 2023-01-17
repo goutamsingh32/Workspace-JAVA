6c9f4c3dbc87638c8f2c5c0267df9dbb768c80c1
goutam singh <goutamsinghchouhan32@gmail.com>
2023-01-17 16:25:45 +0530

    throw,Ducking the exception throws

--- Exceptions.java
+++ Exceptions.java
@@ -31,3 +31,30 @@ public class Exceptions{
 
     }
 }
+
+//******************************************* */
+
+//  Throws keyword
+class A {
+
+    public void show() throws ClassNotFoundException{
+        Class.forName("Demo");
+    }
+}
+
+public class Exceptions{
+
+    static{
+        System.out.println("class loaded");
+    }
+
+    public static void main(String args[]){
+        A obj = new A();
+        try {
+            obj.show();
+        } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
